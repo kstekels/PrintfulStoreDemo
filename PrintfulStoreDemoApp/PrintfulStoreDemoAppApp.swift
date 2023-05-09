6b3f0e6aa81c13bc9b96445748155f2c7932f9c7
kstekels <kstekels+github@gmail.com>
2023-05-09 15:55:54 +0300

    Initial Commit

---
+++ PrintfulStoreDemoApp/PrintfulStoreDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrintfulStoreDemoAppApp.swift
+//  PrintfulStoreDemoApp
+//
+//  Created by Kārlis Štekels on 09/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct PrintfulStoreDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
