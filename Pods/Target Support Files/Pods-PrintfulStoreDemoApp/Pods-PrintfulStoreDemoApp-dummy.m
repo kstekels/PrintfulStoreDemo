08fa6512d27ff40b635fb8608673c2eeb66ca742
kstekels <kstekels+github@gmail.com>
2023-05-15 01:02:43 +0300

    Delete pods

--- Pods/Target Support Files/Pods-PrintfulStoreDemoApp/Pods-PrintfulStoreDemoApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PrintfulStoreDemoApp : NSObject
-@end
-@implementation PodsDummy_Pods_PrintfulStoreDemoApp
-@end
--- Pods/Target Support Files/SkeletonUI/SkeletonUI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SkeletonUI : NSObject
-@end
-@implementation PodsDummy_SkeletonUI
-@end
